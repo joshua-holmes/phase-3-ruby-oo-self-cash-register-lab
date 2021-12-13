c27bf08a15628af7915468261998d80ecd5e1686
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2021-12-13 00:05:06 -0600

    commit tree 2d3c2dcf91d0e95380738a6794486352ae2707d6

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
-
+class CashRegister
+    attr_accessor :total, :discount
+    attr_reader :items
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @prevTotal = 0
+        @items = []
+    end
+    def add_item(title, price, quantity = 1)
+        quantity.times do
+            self.items.push(title)
+        end
+        @prevTotal = self.total
+        self.total += price * quantity
+    end
+    def apply_discount
+        if self.discount == 0
+            "There is no discount to apply."
+        else
+            self.total = (self.total * (100 - self.discount) / 100.0).to_i
+            "After the discount, the total comes to $#{self.total}."
+        end
+    end
+    def void_last_transaction
+        self.total = @prevTotal
+    end
+end
